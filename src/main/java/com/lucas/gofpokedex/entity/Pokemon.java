6af8898d07dc96fbf81a0c88a9fce21a75994bc4
Lucas Oliveira <luc.osilva@gmail.com>
2026-06-20 20:53:08 -0300

    add Pokemon entity

---
+++ src/main/java/com/lucas/gofpokedex/entity/Pokemon.java
@@ -0,0 +1,35 @@
+package com.lucas.gofpokedex.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Pokemon {
+    @Id
+    private Long id;
+    @Column(unique = true, nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private int height;
+    @Column(nullable = false)
+    private int weight;
+    private List<String> types;
+    private List<String> abilities;
+    @Column(nullable = false)
+    private int attack;
+    @Column(nullable = false)
+    private int defense;
+    @Column(nullable = false)
+    private int speed;
+    @Column(name = "base_experience", nullable = false)
+    private int baseExperience;
+}
