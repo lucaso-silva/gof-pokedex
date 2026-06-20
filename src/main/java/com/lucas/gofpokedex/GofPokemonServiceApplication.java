8ae3dfaa902aa68a8661216b88bb62c6fa84ee9d
Lucas Oliveira <luc.osilva@gmail.com>
2026-06-20 15:36:34 -0300

    initial project folder structure

---
+++ src/main/java/com/lucas/gofpokedex/GofPokemonServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.gofpokedex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GofPokemonServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GofPokemonServiceApplication.class, args);
+	}
+
+}
